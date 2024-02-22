2ff3283851bc05b9bf3807ead6de874b2e0b477a
Seid Redi <seidredi7@gmail.com>
2024-02-22 16:47:46 -0600

    added a reflection route for accounta buddy router

--- server/routes/accounta_buddy.router.js
+++ server/routes/accounta_buddy.router.js
@@ -39,7 +39,7 @@ router.get('/:accounta_buddy_id', (req, res) => {
  * GET action_plan for an accounta_buddy 
  */
 router.get('/actionplan/:goal_id', (req,res)=>{
-    const queryText = `select action_plan.action_plan_title, 
+    const queryText = `select  action_plan.action_plan_title, 
                         action_plan.action_plan_desc, 
                         action_plan.target_date, 
                         action_plan.date_created, 
@@ -54,11 +54,21 @@ router.get('/actionplan/:goal_id', (req,res)=>{
 });
 
 
-  /**
- * POST route template
+ /**
+ * GET reflection for an accounta_buddy 
  */
-router.post('/', (req, res) => {
-  // POST route code here
+router.get('/reflection/:goal_id',(req,res)=>{
+    const queryText = `select reflection.id, 
+                        reflection.reflection_title, 
+                        reflection.reflection_desc, 
+                        reflection.date_created, 
+                        reflection.date_modified from reflection 
+                        join goal on goal.id = reflection.goal_id where goal.id = ${req.params.goal_id}`;
+    pool.query(queryText).then((result)=>{
+        res.send(result.rows);
+    }).catch((error)=>{
+        console.error(error);
+    })
 });
 
 module.exports = router;
