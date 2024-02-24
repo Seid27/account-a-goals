dcfd188a7ab49fdda17c69e77962647b0456b736
Seid Redi <seidredi7@gmail.com>
2024-02-24 17:27:13 -0600

    added a addGoalsSaga to add a goal

--- src/redux/sagas/goals.saga.js
+++ src/redux/sagas/goals.saga.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { put, takeLatest } from 'redux-saga/effects';
+import { put, takeEvery, takeLatest } from 'redux-saga/effects';
 
 function* fetchGoals() {
 
@@ -10,6 +10,15 @@ function* fetchGoals() {
         console.log(error);
     }
     
+}
+
+function* addGoalSaga(action){
+    try {
+        yield axios.post('/api/goals', action.payload);
+        yield takeLatest('FETCH_GOALS', fetchGoals);
+    } catch (error) {
+        console.error(error);
+    }
 }
 
 function* goalsSaga() {
