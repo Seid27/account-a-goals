ee1532530a8c1c232cfbec9b6d5805cdf78f0386
Seid Redi <seidredi7@gmail.com>
2024-04-25 22:55:54 -0500

    added a refactored add dialog to support adding a goal

--- src/components/Goals/Goals.jsx
+++ src/components/Goals/Goals.jsx
@@ -1,15 +1,19 @@
 import { Box,Checkbox, ListItem, ListItemButton, ListItemIcon, ListItemText} from '@mui/material';
 import List from '@mui/material/List';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import DeleteDialog from './Dialogs/DeleteDialog';
 import AddDialog from './Dialogs/AddDialog';
+import DateSelector from '../Misc/DateSelector';
+import StatusSelector from '../Misc/StatusSelector';
+import Search from '../Search/Search';
 
 // This is goals page
 // It displays a lsit of goals for a user
 export default function Goals({goals}) {
     const dispatch = useDispatch();
     const history = useHistory();
+    const user = useSelector((store) => store.user);
     // const goals = useSelector(s=>s.goals);
 
     //fetch all data for a user
@@ -67,7 +71,7 @@ export default function Goals({goals}) {
             <Box sx={{ width: '100%', maxWidth: 800,}}>
                 <Box sx={{p:2,
                         display: 'flex', alignItems:'center', justifyContent:'space-between'}}>
-                    <AddDialog
+                    {/* <AddDialog
                         title={'Add a Goal'}
                         label={{
                             title: 'Title',
@@ -79,7 +83,16 @@ export default function Goals({goals}) {
                             description: 'goal_description',
                             targetDate: 'target_date'
                         }}
-                        action='ADD_GOAL'/>
+                        action='ADD_GOAL'/> */}
+                    <AddDialog
+                        dialogTitle={'Add a Goal'}
+                        id = {user.id}
+                        action='ADD_GOAL'>
+                            <Search/>
+                            <DateSelector/>
+                            <StatusSelector/>
+                    </AddDialog>
+                    
                 </Box>
                 
                 <List>
